6c5cfcda51a48bc312ed8b02f6d568f892763d21
Chang Liu <changliu@microsoft.com>
2021-04-06 19:52:48 +0800

    prepare-ubuntu.sh updated

--- prepare-ubuntu.sh
+++ prepare-ubuntu.sh
@@ -2,19 +2,17 @@ sudo add-apt-repository ppa:jonathonf/vim # latest but unofficial package for Vi
 sudo apt update
 sudo apt install --upgrade python3 python3-pip git tmux vim
 
-sudo pip3 install numpy torch matplotlib jupyter tqdm tabulate # scipy
-sudo pip3 install dalib sklearn
-
-git config --global user.email "changliu@microsoft.com"
-git config --global user.name "Chang Liu"
+sudo pip3 install numpy torch torchvision matplotlib jupyter
+# sudo pip3 install sklearn scipy tqdm tabulate dalib
 
 mkdir ~/.ssh # run on local: `cat ~/.ssh/id_ed25519.pub | ssh username@hostname 'cat >> .ssh/authorized_keys'`
 
 # git clone https://github.com/changliu00/config.git
-rm ~/.bashrc
+rm -f ~/.bashrc
 ln -s ~/config/bashrc ~/.bashrc
 ln -s ~/config/tmux.conf ~/.tmux.conf
 ln -s ~/config/vimrc ~/.vimrc
+ln -s ~/config/gitconfig ~/.gitconfig
 
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
